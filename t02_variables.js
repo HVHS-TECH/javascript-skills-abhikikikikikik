5c0f4858c62567b6256dbe0dfbf5dd93042cfe08
John Doe <johndoe@example.com>
2026-04-29 12:29:47 +1200

    Commit sync

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,21 @@
-/* Paste your code from task 1 here */
+/****************************
+Header
+****************************/
+
+console.log("Hello, World!");
+console.log("My name is Abhik.");
+console.log("I am 15 years old.");
+console.log("pocketMoney");
+
+// Variables
+
+let pocketMoney = 3;
+console.log("You have " + pocketMoney + " dollars");
+
+/******************************
+ Main code
+ ******************************/
+
+/******************************
+Functions
+ ******************************/
