2e76776a31f4a3c05b1eae11d07566c402b229cc
John Doe <johndoe@example.com>
2026-05-07 13:06:03 +1200

    Commit changes

--- t03_maths.js
+++ t03_maths.js
@@ -3,11 +3,24 @@ Header
 ****************************/
 
 // Variables
+let age = 15;
+let name = Hello;
+let money = 15;
+let year = 2026;
+var answer;
 
 /******************************
  Main code
+ 
  ******************************/
-
+answer = num1 + num2;
+console.log("add", answer);
+answer = num1 - num2;
+console.log("subtract", answer);
+answer = num1 * num2;
+console.log("multiply", answer);
+answer = num1 / num2;
+console.log("divide", answer);
 /******************************
 Functions
  ******************************/
--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,23 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Header
+****************************/
+<div id="JavaScriptOutput">
+  <p>This will be replaced</p>
+</div>;
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added by Javascript</h2>";
+
+// Variables
+var num1 = 10;
+var num2 = 5;
+var answer;
+
+/******************************
+ Main code
+ ******************************/
+OUTPUT.innerHTML;
+/******************************
+ *=
+Functions
+ ******************************/
